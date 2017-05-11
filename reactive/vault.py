0ba3a38de9dc8019e7347781541244a3de0057e5
Paul Collins <paul.collins@canonical.com>
2017-05-11 13:45:46 +1200

    write vault.hcl to the correct path

--- reactive/vault.py
+++ reactive/vault.py
@@ -19,14 +19,16 @@ from charms.reactive import (
     when_not,
 )
 
+
 @when('snap.installed.vault')
 @when_not('configured')
 def configure_vault():
-    render('vault.hcl.j2', '/var/snap/common/vault/vault.hcl', {}, perms=0o644)
+    render('vault.hcl.j2', '/var/snap/vault/common/vault.hcl', {}, perms=0o644)
     render('vault.service.j2', '/etc/systemd/system/vault.service', {}, perms=0o644)
     service_restart('vault')
     open_port(8200)
     set_state('configured')
+
 
 @hook('upgrade-charm')
 def upgrade_charm():
